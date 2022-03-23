0fc195cfde0fdaf948fc9bd785fb549ec9096a46
CharlotteBevan <101196578+CharlotteBevan@users.noreply.github.com>
2022-03-23 17:31:26 +0000

    BACK BUTTONS fix and third branching

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 // Run this code when a form is submitted to 'level-activity-answer'
 router.post('/v2/level-activity-answer', function (req, res) {
 
-    // Make a variable and give it the value from 'where-do-you-live'- this is pulled from ID
+    // Make a variable and give it the value from 'where-do-you-live'- this is pulled from Name
     var Location = req.session.data['activity-level']
      // Check whether the variable matches a condition - this is pulled from VALUE
     if (Location == "I don't exercise at the moment"){
@@ -13,7 +13,7 @@ router.post('/v2/level-activity-answer', function (req, res) {
       res.redirect('/v2/motivation')
     } else {
       // Send user to ineligible page
-      res.redirect('/v2/goals')
+      res.redirect('/v2/exercise-preferences')
     }
    })
 
@@ -21,7 +21,7 @@ router.post('/v2/level-activity-answer', function (req, res) {
 // Run this code when a form is submitted to 'level-activity-answer'
 router.post('/v2/additional-support-groups-answer', function (req, res) {
 
-  // Make a variable and give it the value from 'where-do-you-live'- this is pulled from ID
+  // Make a variable and give it the value from 'where-do-you-live'- this is pulled from Name
   var Location = req.session.data['would-you-like-information-on-support-groups-and-networks']
    // Check whether the variable matches a condition - this is pulled from VALUE
   if (Location == "Yes"){
@@ -32,5 +32,21 @@ router.post('/v2/additional-support-groups-answer', function (req, res) {
     res.redirect('/v2/check-answers')
   }
  })
+
+    // Add your routes here - above the module.exports line
+// Run this code when a form is submitted to 'level-activity-answer'
+router.post('/v2/goals-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'where-do-you-live'- this is pulled from Name
+    var Location = req.session.data['goals']
+     // Check whether the variable matches a condition - this is pulled from VALUE
+    if (Location == "Lose weight"){
+      // Send user to next page
+      res.redirect('/v2/height')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/v2/level-activity')
+    }
+   })
  
 module.exports = router
