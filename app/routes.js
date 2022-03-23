0c36a193cd7991a14e70906e8e85e6ed96cd3360
Jess <jessica.mancoo@hippodigital.co.uk>
2022-03-23 16:00:41 +0000

    branching
    branching

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 // Run this code when a form is submitted to 'level-activity-answer'
-router.post('level-activity-answer', function (req, res) {
+router.post('/v2/level-activity-answer', function (req, res) {
 
     // Make a variable and give it the value from 'where-do-you-live'
-    var Location = req.session.data['activity-level-1']
+    var Location = req.session.data['activity-level']
      // Check whether the variable matches a condition
-    if (Location == "Few times a week"){
+    if (Location == "I don't exercise at the moment"){
       // Send user to next page
-      res.redirect('/error')
+      res.redirect('/v2/motivation')
     } else {
       // Send user to ineligible page
-      res.redirect('/question-2')
+      res.redirect('/v2/goals')
     }
    })
  
