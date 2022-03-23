478d5bfb2cc7d71a0b0af5d9a8267a236640a70b
CharlotteBevan <101196578+CharlotteBevan@users.noreply.github.com>
2022-03-23 16:04:39 +0000

    added ID and NAME in routes

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,9 @@ const router = express.Router()
 // Run this code when a form is submitted to 'level-activity-answer'
 router.post('/v2/level-activity-answer', function (req, res) {
 
-    // Make a variable and give it the value from 'where-do-you-live'
+    // Make a variable and give it the value from 'where-do-you-live'- this is pulled from ID
     var Location = req.session.data['activity-level']
-     // Check whether the variable matches a condition
+     // Check whether the variable matches a condition - this is pulled from VALUE
     if (Location == "I don't exercise at the moment"){
       // Send user to next page
       res.redirect('/v2/motivation')
