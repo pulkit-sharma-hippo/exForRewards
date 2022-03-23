cd033fe15b58bd1fc08fb3177e2656a912d41586
Vanessa Kirby-Firth <vanessa.kirby-firth@hippodigital.co.uk>
2022-03-23 16:53:53 +0000

    Added branching

--- app/routes.js
+++ app/routes.js
@@ -16,5 +16,21 @@ router.post('/v2/level-activity-answer', function (req, res) {
       res.redirect('/v2/goals')
     }
    })
+
+   // Add your routes here - above the module.exports line
+// Run this code when a form is submitted to 'level-activity-answer'
+router.post('/v2/additional-support-groups-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'where-do-you-live'- this is pulled from ID
+  var Location = req.session.data['would-you-like-information-on-support-groups-and-networks']
+   // Check whether the variable matches a condition - this is pulled from VALUE
+  if (Location == "Yes"){
+    // Send user to next page
+    res.redirect('/v2/personal-details3')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/v2/check-answers')
+  }
+ })
  
 module.exports = router
